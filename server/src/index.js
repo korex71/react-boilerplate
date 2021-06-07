0e01fe1c15577266d8dfce20b7f8f4bc4b9369a5
korex71 <kore.dv7@gmail.com>
2021-06-07 07:23:57 -0300

    Add cors & remove ytgetter

--- server/src/index.js
+++ server/src/index.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const app = express();
 const Routes = require("./routes");
+const cors = require("cors");
 const PORT = process.env.PORT || 3333;
 
+app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json({ extended: true }));
 app.use(Routes);
