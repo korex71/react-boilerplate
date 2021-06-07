784d4dc8621e776fd88df241c200889ae59ff050
korex71 <kore.dv7@gmail.com>
2021-06-07 08:02:27 -0300

    Remake Hello UI

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,33 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Home from "./components/Home";
 import { AppContext } from "./contexts/AppContext";
+import styled from "styled-components";
+
+export const Wrapper = styled.div`
+  background: ${({ color }) => color};
+  color: white;
+  height: 100vh;
+`;
 
 function App() {
   const [count, setCount] = useState(0);
+  const [color, setColor] = useState("#202020");
+
+  function getRandomInt(min, max) {
+    return min + Math.floor(Math.random() * (max - min + 1));
+  }
+
+  useEffect(() => {
+    const darkPalette = [
+      "#202020",
+      "#151515",
+      "#301B3F",
+      "#161616",
+      "#3C415C",
+      "#231E23",
+    ];
+    setColor(darkPalette[getRandomInt(0, 5)]);
+  }, [count]);
 
   return (
     <AppContext.Provider
@@ -12,7 +36,9 @@ function App() {
         setCount,
       }}
     >
-      <Home />
+      <Wrapper color={color}>
+        <Home />
+      </Wrapper>
     </AppContext.Provider>
   );
 }
--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -1,27 +1,30 @@
 import { useContext } from "react";
 import { AppContext } from "../../contexts/AppContext";
-import { Wrapper } from "./styles";
+import { Container, Button, Col, Row } from "react-bootstrap";
 
 export default function Home() {
   const { count, setCount } = useContext(AppContext);
+
   return (
-    <Wrapper className="d-flex flex-column justify-content-center align-items-center">
-      <h1>Hello world</h1>
-      Count: {count}
-      <div className="row py-3">
-        <button
-          onClick={() => setCount(count + 1)}
-          className="col px-5 mx-2 btn btn-success"
-        >
-          +
-        </button>
-        <button
-          onClick={() => setCount(count - 1)}
-          className="col px-5 mx-2 btn btn-danger"
-        >
-          -
-        </button>
-      </div>
-    </Wrapper>
+    <Container className="d-flex justify-content-center align-items-center vh-100">
+      <Col>
+        <h1 className="text-center">Hello world</h1>
+        <p className="text-center">{count}</p>
+        <Row className="py-3 justify-content-center">
+          <Button
+            onClick={() => setCount(count + 1)}
+            className="col-1 px-5 mx-2 btn btn-success"
+          >
+            +
+          </Button>
+          <Button
+            onClick={() => setCount(count - 1)}
+            className="col-1 px-5 mx-2 btn btn-danger"
+          >
+            -
+          </Button>
+        </Row>
+      </Col>
+    </Container>
   );
 }
